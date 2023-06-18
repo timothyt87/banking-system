7abdafec6b083f1e2e8707b32fdd2c12c2d5b8d3
Timothy <timothyt87@gmail.com>
2023-06-18 21:30:47 +0700

    Add basic transfer_fund function to simulate fund transfer

--- backend/src/api/v1/transactions/router.py
+++ backend/src/api/v1/transactions/router.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import APIRouter
 from .utils import generate_trx_id
 
@@ -8,7 +10,31 @@ transactions_router = APIRouter(
 )
 
 
-# index route
+# get all transactions
 @transactions_router.get('/')
 async def index():
     return await generate_trx_id()
+
+
+# Create new transactions
+@transactions_router.post(
+    path="/",
+    description="Transfer fund to another account"
+)
+async def transfer_fund(
+        sender_account_number: int,
+        recipient_account_number: int,
+        amount: int
+):
+    trx_fee: int = 6500
+    total_fund_deduct = amount + trx_fee
+    return {
+        "trx_id": await generate_trx_id(),
+        "trx_timestamp": datetime.datetime.now(),
+        "sender_account_number": sender_account_number,
+        "recipient_account_number": recipient_account_number,
+        "amount": amount,
+        "trx_fee": trx_fee,
+        "total_fund_deduct": total_fund_deduct,
+        "transfer_method": "immediate"
+    }
